8d0905ff24d0ba07fa700d28bd432007542a9c7b
Frederick Charbonnier <frederickcharbonnier@icloud.com>
2025-12-09 17:37:44 +0100

    Finished ex00 and begun 01

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 12:46:06 by fredchar          #+#    #+#             */
+/*   Updated: 2025/12/09 16:49:33 by fredchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string _name;
+		int _hp;
+		int _ep;
+		int _atkdmg;
+	public:
+		ClapTrap(std::string name);
+		~ClapTrap();
+		ClapTrap& operator=(const ClapTrap& other);
+		ClapTrap(const ClapTrap& other);
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+};
